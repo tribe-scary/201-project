42ba1e8388f94636a80e853f16f00dbd373cb63c
Junyoung Son <junyoungson808@gmail.com>
2022-08-01 10:31:35 -0700

    rebase

--- js/app.js
+++ js/app.js
@@ -1,14 +1,1 @@
-"use strict";
-
-const cvs = document.getElementById("myCanvas");
-const ctx = cvs.getContext("2d");
-
-function draw() {}
-
-function loop() {
-  draw();
-
-  requestAnimationFrame(loop);
-}
-
-loop();
+'use strict';
