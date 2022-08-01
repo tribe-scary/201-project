b3189457314f10785b770c8bac03ca23c37e9c00
Adrian Butler Jr <butlerjr123@gmail.com>
2022-08-01 17:06:54 -0500

    added player object and save functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 'use strict';
+console.log("hello");
+
+Player.players = [];
+
+function Player(name, score)
+{
+  this.name = name;
+  this.score = score;
+
+  Player.players.push(this);
+}
+
+Player.saveToLocalStorage = function ()
+{
+  let data = JSON.stringify(Player.players);
+  localStorage.setItem('players', data);
+};
+
+Player.readFromLocalStorage = function ()
+{
+  let data = JSON.parse(localStorage.getItem('players'));
+  console.log(data);
+  for (let i = 0; i < data.length; i++)
+  {
+    let playerName = data[i].name;
+    let playerScore = data[i].score;
+    new Player(playerName, playerScore);
+  }
+};
+
+
--- js/game.js
+++ js/game.js
@@ -322,7 +322,7 @@ const pipes = {
         score.value += 1;
         SCORE_S.play();
         score.best = Math.max(score.value, score.best);
-        localStorage.setItem('best', score.best);
+        localStorage.setItem('best', score.best); // Todo save highscore to player object with name instead of this
       }
     }
   },
