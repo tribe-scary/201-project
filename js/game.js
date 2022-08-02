1a88bdcde4020edf2c00660b1c41d20b4ff719eb
Adrian Butler Jr <butlerjr123@gmail.com>
2022-08-02 15:51:30 -0500

    added name input

--- js/game.js
+++ js/game.js
@@ -406,7 +406,6 @@ function loop()
     requestAnimationFrame(loop);
   } else
   {
-    Player.readFromLocalStorage();
     let nameInput = document.createElement('input');
     gameAreaElement.appendChild(nameInput);
     nameInput.type = 'text';
@@ -416,8 +415,7 @@ function loop()
     {
       if (event.key === 'Enter')
       {
-        new Player(nameInput.value, score.value);
-        Player.saveToLocalStorage();
+        Player.saveToLocalStorage(new Player(nameInput.value, score.value));
         gameAreaElement.removeChild(nameInput);// safely deletes nameInput
         nameInput.replaceWith(nameInput.cloneNode());// removes event listener
       }
